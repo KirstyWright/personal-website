4478005c55c89f7c5fc46272db4163a230f717b1
KirstyWright <kirsty@evecmdr.com>
2018-06-19 19:48:32 +0100

    Removed typed.js. Console is now mostly native js now

--- components/Console.vue
+++ components/Console.vue
@@ -9,42 +9,83 @@
         </div>
         <div class="terminal__body">
             <span class="terminal__body--animated-content" id='terminalType'></span>
-            <div id="terminal__results-area"></div>
-            <div v-show='false' class='terminal__input-row'><span class='terminal__body-prefix'>guest$</span><span id='terminal__user-input'> </span><span class="terminal__cursor">|</span></div>
+            <div id="terminal__results-area">
+                <p class="mb-0" v-bind:key="index" v-for="(row,index) in output"><span class='terminal__body-prefix' v-if="row.type==='command'">guest$&nbsp;</span><span v-html="row.content"></span></p>
+            </div>
+            <div v-show='false' class='terminal__input-row'><span class='terminal__body-prefix'>guest$&nbsp;</span><span id='terminal__user-input'></span><span class="terminal__cursor">|</span></div>
         </div>
     </div>
 </template>
 
 <script>
-import Typed from 'typed.js';
-
 export default {
+    data: function(){
+        return {
+            'output':[]
+        }
+    },
     mounted: function(){
         $('.terminal').height(window.innerHeight - $('.navbar').height() - 70);
-        var terminalTyped = new Typed('#terminalType', {
-            strings: [
-                'Hi, my name is Kirsty and I am a backend web developer.^500 <br><br>Welcome to my personal website. ^500 <br><br> <a href="https://github.com/KirstyWright" target="_blank">GitHub</a>, <a href="https://www.linkedin.com/in/kirstywrightweb/" target="_blank">Linkedin</a>, <a href="mailto:kirsty@evecmdr.com">Email</a><br>'
-            ],
-            typeSpeed: 30,
-            backspeed: 0,
-            backDelay: 3000,
-            loop: false,
-            showCursor: false,
-            onComplete: (self) => {
-                $('.terminal__input-row').show();
-            }
-        });
-        document.onkeydown=function(e){
+        $('.terminal__input-row').show();
+        document.onkeydown = (e) => {
             let key = e.keyCode || e.charCode;
+            let input = document.getElementById('terminal__user-input');
             if (key == 13) {
-                console.log('ENTER');
-                $('#terminal__results-area').html($('#terminal__results-area').html()+'<p>'+$('.terminal__body-prefix').html()+$('#terminal__user-input').html()+'</p>');
-                $('#terminal__user-input').html('');
+                this.output.push({
+                    content: input.innerHTML,
+                    type: 'command'
+                })
+                let response = this.parseCommand(input.innerHTML);
+                input.innerHTML = "";
+                this.output.push({
+                    content: this.runCommand(response['command'],response['args']),
+                    type: response
+                })
             } else if (key == 8 || key == 46) {
                 e.preventDefault();
-                $('#terminal__user-input').html($('#terminal__user-input').html().slice(0,-1))
-            } else {
-                $('#terminal__user-input').html($('#terminal__user-input').html()+e.key)
+                input.innerHTML = input.innerHTML.slice(0,-1)
+            } else if (key >= 32 && key <= 126) {
+                input.innerHTML = input.innerHTML + e.key
+            }
+        }
+    },
+    methods: {
+        parseCommand: function(line) {
+            let words = line.split(' ');
+            return {
+                command: (words.length > 0 ? words[0] : ''),
+                args: words.splice(1)
+            }
+        },
+        runCommand: function(command,args) {
+            switch (command) {
+                case 'clear':
+                    this.output = [];
+                    break;
+                case 'hi':
+                    return 'hello';
+                case 'goto':
+                    return '<a href="https://xkcd.com/292/" target="_blank">Really you tried to goto ?</a>';
+                case 'open':
+                    let pages = ['projects','cv'];
+                    if (pages.includes(args[0])) {
+                        window.location.href = '/'+args[0];
+                        return
+                    } else {
+                        return 'invalid page. Pick from: '+pages.join(', ');
+                    }
+                case 'help':
+                    let commands = [
+                        '-- Commands are listed below --',
+                        'clear: clears the console',
+                        'hi: says hello',
+                        'goto: just try it',
+                        'open: opens a page on this website'
+                    ];
+                    return commands.join('<br>');
+                default:
+                    return command+" is not a recognised command"
+
             }
         }
     }
