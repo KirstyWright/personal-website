f48ceab9e4b38adbb29c07cdf1d7a483d5c66291
KirstyWright <kirsty@evecmdr.com>
2018-06-25 21:14:32 +0100

    changed styling of me

--- components/About.vue
+++ components/About.vue
@@ -3,7 +3,7 @@
         <h1 class='text-center'>About me</h1>
         <div class="project-card" >
             <div class="d-inline-flex">
-                <img src="~/assets/me.jpg" style='display:block;width:200px;height:200px;border-radius:5px;border:2px solid #000;' alt="Picture of Kirsty" class='mr-3'>
+                <img src="~/assets/me.jpg" style='display:block;width:200px;height:200px;' alt="Picture of Kirsty" class='mr-3'>
                 <div>
                     <p>My name is Kirsty and I am passionate developer, focusing on backend web systems but equally at home with a range of projects.</p>
                     <p>I endevour to be proud of all my projects &amp; work.</p>
